4cbd7d7e4c4ae52422b50eafe1a66513744adc5b
daFi29988 <daniel29@outlook.it>
2023-01-11 22:21:05 +0100

    http request demo completata

---
+++ http-demo/src/components/survey/UserExperiences.vue
@@ -0,0 +1,70 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Submitted Experiences</h2>
+      <div>
+        <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
+      </div>
+      <p v-if="isLoading">Caricamento...</p>
+      <p v-else-if="!isLoading && (!results || resuls.length === 0)">Non ci sono feedback salvati, comincia aggiungendone uno</p>
+      <ul v-else-if="!isLoading && results && data.length > 0">
+        <survey-result
+          v-for="result in results"
+          :key="result.id"
+          :name="result.name"
+          :rating="result.rating"
+        ></survey-result>
+      </ul>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import SurveyResult from './SurveyResult.vue';
+
+export default {
+  components: {
+    SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+      isLoading: false
+    }
+  },
+  methods: {
+    loadExperiences() {
+      this.isLoading = true;
+      fetch("https://vue-http-demo-47f5e-default-rtdb.europe-west1.firebasedatabase.app/surveys.json")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      }
+      ).then((data) => {
+        this.isLoading = false;
+        const results = [];
+        for (const id in data) {
+          results.push({ 
+            id: id, 
+            name: data[id].name, 
+            rating: data[id].rating,
+           });
+        }
+        this.results = results;
+      }); 
+    },
+  },
+  mounted() {
+    this.loadExperiences();
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
